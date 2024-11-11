e308de0567e361487f078d49004e9f469f9924b9
Eduardo <grutzmann9@gmail.com>
2024-11-11 12:50:07 -0300

    responsive image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,13 +9,12 @@ const Home = () => {
             <div className='text text-light fs-3 mb-4 mb-md-0 me-4'>
                 DocKing is an intuitive and efficient <b>molecular docking tool</b> designed for researchers and professionals in the fields of biotechnology and pharmacology. It allows for the visualization, analysis, and interaction with molecular models, streamlining the process of discovering new molecular interactions. With a simple interface and advanced features, DocKing provides a platform to optimize the study of ligands and their interactions with target proteins, aiding in the acceleration of scientific research and the development of new drugs.
             </div>
-            <img 
-              src={Print} 
-              alt="Print do aplicativo" 
-              className='img-fluid' 
-              id='print' 
-              style={{ maxWidth: '100%', height: 'auto' }} 
-            />   
+              <img 
+                src={Print} 
+                alt="Print do aplicativo" 
+                className='img-fluid' 
+                id='print' 
+              />          
         </div>  
     </div>
   );
